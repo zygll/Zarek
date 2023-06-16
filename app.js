9e5c154608844e53b38691e5e932f93667fb62e8
c-liaol02 <1193057854@qq.com>
2023-06-16 09:49:05 +0800

    fix

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-/**
- * Date : 2019.12.01
- * Author : 丸子团队（波波、Chi、ONLINE.信）
- * Github 地址: https://github.com/dchijack/Travel-Mini-Program
- * GiTee 地址： https://gitee.com/izol/Travel-Mini-Program
- */
 const API = require('/utils/base')
-
 App({
 
   onLaunch: function () {
@@ -17,6 +10,40 @@ App({
         this.globalData.CustomBar = e.platform == 'android' ? e.statusBarHeight + 50 : e.statusBarHeight + 45;
       }
     })
+
+      wx.loadFontFace({
+        family: 'Nabla', //设置一个font-family使用的名字 中文或英文
+        global: true,//是否全局生效
+        source: 'url("https://zygll.com/wp-content/uploads/Nabla.ttf")', //字体资源的地址
+        success: function(e){
+          console.log('Nabla-字体调用成功')
+        },
+        fail: function (e) {
+          console.log('字体调用失败')
+        },
+      })
+      wx.loadFontFace({
+        family: 'JinBuTi', //设置一个font-family使用的名字 中文或英文
+        global: true,//是否全局生效
+        source: 'url("https://zygll.com/wp-content/uploads/JinBuTi.ttf")', //字体资源的地址
+        success: function(e){
+          console.log('JinBuTi-字体调用成功')
+        },
+        fail: function (e) {
+          console.log('字体调用失败')
+        },
+      })
+      wx.loadFontFace({
+        family: 'SmileySans', //设置一个font-family使用的名字 中文或英文
+        global: true,//是否全局生效
+        source: 'url("https://zygll.com/wp-content/uploads/SmileySans.ttf")', //字体资源的地址
+        success: function(e){
+          console.log('SmileySans-字体调用成功')
+        },
+        fail: function (e) {
+          console.log('字体调用失败')
+        },
+      })
   },
 
   onShow: function () {
--- pages/index2/index2.js
+++ pages/index2/index2.js
@@ -1,15 +1,79 @@
-/**
- * Author : 丸子团队（波波、Chi、ONLINE.信）
- * Github 地址: https://github.com/dchijack/Travel-Mini-Program
- * GiTee 地址： https://gitee.com/izol/Travel-Mini-Program
- */
+import * as echarts from '../../ec-canvas/echarts';
+let chart = null;
+function initChart(canvas, width, height, dpr) {
+  chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    graphic: {
+      elements: [
+        {
+          type: 'text',
+          left: 'center',
+          top: 'center',
+          style: {
+            text: 'Zreak',
+            fontSize: 30,
+            fontWeight: 'bold',
+            lineDash: [0, 100],
+            lineDashOffset: 0,
+            fill: 'transparent',
+            stroke: '#9ff7ba',
+            lineWidth: 1
+          },
+          keyframeAnimation: {
+            duration:10000,
+            loop: true,
+            keyframes: [
+              {
+                percent: 0.7,
+                style: {
+                  fill: '#ffc250',
+                  lineDashOffset: 200,
+                  lineDash: [300, 0]
+                }
+              },
+              {
+                // Stop for a while.
+                percent: 0.8,
+                style: {
+                  fill: '#3ff7f9'
+                }
+              },
+              {
+                percent: 1,
+                style: {
+                  fill: '#5200b9'
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+//以上logo部分
 const API = require('../../utils/api')
-
+var timer = null;
 Page({
-
   data: {
+    ec: {
+      onInit: initChart // 渲染logo
+    },
     posts: [],
-    page:1,
+    text: "",
+    typing: "",
+    description: "长到这么大，我说不出来我最爱的一部电影",
+    interval: 20,
+    page: 1,
     siteInfo: '',
     indicatorDots: !1,
     autoplay: !0,
@@ -23,7 +87,24 @@ Page({
   },
 
   onLoad: function () {
-    let that=this;
+    console.log('onLoad');
+    let that = this;
+    // var index = 0;
+    // var temp = "";
+    // timer = setInterval(function () {
+    //   if (temp.length < init_text.length) {
+    //     temp += init_text[index]
+    //     index = (index + 1);
+
+    //   }else{
+    //   console.log('timer');
+    //   }
+    //   that.setData({
+    //     text: temp
+    //   });
+    // }, that.data.interval);
+
+
     wx.getSystemInfo({
       success: function (a) {
         that.setData({
@@ -31,54 +112,60 @@ Page({
         });
       }
     });
-    this.getSiteInfo();
-    // this.getStickyPosts();
-    this.getCategories({per_page:2});
-    this.getAdvert();
+    this.getCategories({
+      per_page: 2
+    });
+    // this.getAdvert();
     this.getPostList();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-
+  onReady: function () {
+    console.log('onReady');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
+    this.getSiteInfo();
+    // this.getStickyPosts();
+
+
+    console.log('onShow');
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
-
+  onHide: function () {
+    console.log('onHide');
+    clearInterval(timer);
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-
+  onUnload: function () {
+    console.log('onUnload');
   },
 
-  onClear:function(){
+  onClear: function () {
     this.setData({
-      searchKey:'',
+      searchKey: '',
     })
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     this.setData({
-      page:1,
-      posts:[],
+      page: 1,
+      posts: [],
       isLastPage: false
     })
     this.getPostList()
@@ -87,10 +174,10 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
     if (!this.data.isLastPage) {
       this.getPostList({
-        page:this.data.page
+        page: this.data.page
       })
     }
   },
@@ -98,96 +185,117 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     return {
-      title: this.data.siteInfo.name ,
+      title: this.data.siteInfo.name,
       path: '/pages/index/index'
     }
   },
 
-  getSiteInfo: function() {
+  getSiteInfo: function () {
     API.getSiteInfo().then(res => {
-      this.setData({
-        siteInfo: res
+        this.setData({
+          siteInfo: res,
+          description: res.description
+        })
+        this.animate(res)
       })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   },
-
-  onInput: function(e) {
+  animate(res) {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'linear'
+    })
+    that.animation = animation;
+    animation.opacity(1).step();
+    that.setData({
+      animationData: animation.export()
+    })
+    var i = 0;
+    timer = setInterval(function () {
+      if (i <= res.description.length) {
+        that.setData({
+          typing: res.description.slice(0, i++)
+        })
+      }
+    }, 200)
+  },
+  onInput: function (e) {
     this.setData({
       searchKey: e.detail.value
     })
   },
 
-  currentChange: function(e) {
+  currentChange: function (e) {
     this.setData({
       currentSwiper: e.detail.current
     });
   },
 
-  getStickyPosts: function() {
+  getStickyPosts: function () {
     API.getStickyPosts().then(res => {
-      this.setData({
-        stickyPost: res
+        this.setData({
+          stickyPost: res
+        })
       })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   },
 
-  getCategories: function(args) {
+  getCategories: function (args) {
     API.getCategories(args).then(res => {
-      this.setData({
-        category: res
+        this.setData({
+          category: res
+        })
       })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   },
 
-  getPostList: function(data) {
+  getPostList: function (data) {
     API.getPostsList(data).then(res => {
-      let args = {}
-      if (res.length < 10) {
-        this.setData({
-          isLastPage: true,
-          loadtext: '到底啦',
-          showloadmore: false
-        })
-      }
-      if (this.data.isBottom) {
-        args.posts = [].concat(this.data.posts, res)
-        args.page = this.data.page + 1
-      } else {
-        args.posts = [].concat(this.data.posts, res)
-        args.page = this.data.page + 1
-      }
-      this.setData(args)
-      wx.stopPullDownRefresh()
-    })
-    .catch(err => {
-      console.log(err)
-      wx.stopPullDownRefresh()
-    })
+        let args = {}
+        if (res.length < 10) {
+          this.setData({
+            isLastPage: true,
+            loadtext: '到底啦',
+            showloadmore: false
+          })
+        }
+        if (this.data.isBottom) {
+          args.posts = [].concat(this.data.posts, res)
+          args.page = this.data.page + 1
+        } else {
+          args.posts = [].concat(this.data.posts, res)
+          args.page = this.data.page + 1
+        }
+        this.setData(args)
+        wx.stopPullDownRefresh()
+      })
+      .catch(err => {
+        console.log(err)
+        wx.stopPullDownRefresh()
+      })
   },
 
-  getAdvert: function() {
+  getAdvert: function () {
     API.indexAdsense().then(res => {
-      console.log(res)
-      if(res.status === 200) {
-        this.setData({
-          advert: res.data
-        })
-      }
-    })
-    .catch(err => {
-      console.log(err)
-    })
+        console.log(res)
+        if (res.status === 200) {
+          this.setData({
+            advert: res.data
+          })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
   },
 
   bindCateByID: function (e) {
@@ -197,23 +305,23 @@ Page({
     })
   },
 
-  bindCateList:function(){
+  bindCateList: function () {
     wx.switchTab({
       url: '/pages/category/category',
     })
   },
 
-  bindDetail: function(e) {
+  bindDetail: function (e) {
     let id = e.currentTarget.id;
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + id,
     })
   },
 
-  onConfirm:function(e){
-    let s=e.detail.value;
+  onConfirm: function (e) {
+    let s = e.detail.value;
     wx.navigateTo({
-      url: '/pages/list/list?s='+s,
+      url: '/pages/list/list?s=' + s,
     })
   }
 
